7a859103776cf7c468fa4bb2f6f00334c676d85d
Daki CARNHOF <carnhofdaki@gmail.com>
2024-12-10 13:06:22 +0000

    Also POST the tx to mempool.space
    No way to submit a packge via mempool.space API yet.

--- submit.sh
+++ submit.sh
@@ -5,4 +5,5 @@ bitcoin-cli -stdin submitpackage < package
 ls tx/???? | while read tx
 do
   cat $tx | bitcoin-cli -stdin sendrawtransaction
+  cat $tx | curl -X POST -sSLd "@-" "https://mempool.space/api/tx" | grep .
 done
