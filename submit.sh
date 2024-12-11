ea2726ce3a4652cdde0b1320b80b3b40da847241
Daki CARNHOF <carnhofdaki@gmail.com>
2024-12-11 14:03:30 +0000

    Some were mined
    In blocks 874207 and 874230 mined by Ant and MARA respectively.
    
    See https://slipstream.mara.com/

--- submit.sh
+++ submit.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-bitcoin-cli -stdin submitpackage < package \
+out=/tmp/out
+test -s package && bitcoin-cli -stdin submitpackage < package \
   | grep '^    "' | grep -o '[0-9a-f]\+'
 
 ls tx/???? | while read tx
 do
-  cat $tx | bitcoin-cli -stdin sendrawtransaction
-  cat $tx | curl -X POST -sSLd "@-" "https://mempool.space/api/tx" | grep .
+  cat $tx | bitcoin-cli -stdin sendrawtransaction 2>/dev/null
+  cat $tx | curl -X POST -sSLd "@-" "https://mempool.space/api/tx" | grep . > $out
+  grep -qw missingorspent $out && rm $tx || cat $out
 done
+rm $out
